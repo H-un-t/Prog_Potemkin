53435f4f3234721a720e6be0aeab6b68d1553c08
Ladanenko1 <ladanenko99@mail.ru>
2023-10-13 19:24:08 +0300

    PZ2 commit

--- Pz_3/PZ_3.py
+++ Pz_3/PZ_3.py
@@ -1,9 +1,14 @@
 # Вариант 20
-z = int(input("введите первое число >>"))
-x = int(input("введите второе число >>"))
-c = int(input("введите третье число >>"))
-if z*x < 0 or z*c < 0 or x*c < 0 or x*z < 0 or z*c < 0 or c*x < 0:
+i = 0
+while i == 0:
+ try:
+  z = int(input("введите первое число ->"))
+  x = int(input("введите второе число ->"))
+  c = int(input("введите третье число ->"))
+  if z*x < 0 or z*c < 0 or x*c < 0 or x*z < 0 or z*c < 0 or c*x < 0:
     print("<ИСТИНА>")
-else:
+  else:
     print("<ЛОЖЬ>")
-    #
+  i = 1
+ except ValueError:
+    print("нужно ввести число")
